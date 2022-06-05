e348316c86a442a352ff109759412d3b97deddd4
Alex-Rockwell <vin-ag@list.ru>
2022-06-05 21:15:45 +0400

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/movie-app/',
+  base: '/e-commerce-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
