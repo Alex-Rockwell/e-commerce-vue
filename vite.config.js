a08e046dae2ee7bf959c7daf3e95ca95d543514a
Alex-Rockwell <vin-ag@list.ru>
2022-06-05 21:22:47 +0400

    deploy-2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   base: '/e-commerce-vue/',
   plugins: [vue()],
@@ -13,5 +14,3 @@ export default defineConfig({
     }
   }
 })
-
-
