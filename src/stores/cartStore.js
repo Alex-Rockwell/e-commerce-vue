a174cdffef182e5de24b13d2dd52bf2e9d12fa6f
Alex-Rockwell <vin-ag@list.ru>
2022-06-13 00:15:55 +0400

    22.06.13

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -7,23 +7,28 @@ export const useCartStore = defineStore({
   }),
   actions: {
     addCartItem(productItem, colorToCart, sizeToCart) {
+      ////////////////////// Check if item is in the cart /////////////////////////
       if (
         this.cartItems.some((el) => el.id == productItem.id) &&
         productItem.type != "configurable"
       ) {
         this.cartItems.map((el) => {
           if (el.id == productItem.id) {
-            return { ...el, qty: el.qty++ }
+            return { ...el, qty: el.qty++ };
           } else {
-            return el
+            return el;
           }
-        })
-        return
+        });
+        return;
       }
+
+      ////////////////////// Standart case /////////////////////////////////////////
       const itemFinal = { ...productItem, qty: 1 };
+
+      ////////////////////// Configurable case /////////////////////////////////////////
       if (productItem.type == "configurable") {
-        itemFinal["colorToCart"] = colorToCart ? colorToCart : "default (red)";
-        itemFinal["sizeToCart"] = sizeToCart ? sizeToCart : "default (M)";
+        itemFinal["colorToCart"] = colorToCart;
+        itemFinal["sizeToCart"] = sizeToCart;
       }
       this.cartItems = [...this.cartItems, itemFinal];
     },
