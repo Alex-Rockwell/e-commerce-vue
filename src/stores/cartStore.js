ff1c70e8d02bb9f0d6020015eae28231d04e188f
Alex-Rockwell <vin-ag@list.ru>
2022-06-12 14:28:19 +0400

    22.06.12

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,32 +1,32 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
 export const useCartStore = defineStore({
-  id: 'cartData',
+  id: "cartData",
   state: () => ({
-    cartItems: []
+    cartItems: [],
   }),
   actions: {
     addCartItem(cartItem, colorToCart, sizeToCart) {
-      const itemFinal = {...cartItem, qty: 1}
-      if (cartItem.type == 'configurable') {
-        itemFinal['colorToCart'] = colorToCart ? colorToCart : 'default (red)'
-        itemFinal['sizeToCart'] = sizeToCart ? sizeToCart : 'default (M)'
+      const itemFinal = { ...cartItem, qty: 1 };
+      if (cartItem.type == "configurable") {
+        itemFinal["colorToCart"] = colorToCart ? colorToCart : "default (red)";
+        itemFinal["sizeToCart"] = sizeToCart ? sizeToCart : "default (M)";
       }
-      this.cartItems = [...this.cartItems, itemFinal]
+      this.cartItems = [...this.cartItems, itemFinal];
     },
     changeQty(id, qty) {
-      this.cartItems = this.cartItems.map(el => {
+      this.cartItems = this.cartItems.map((el) => {
         if (el.id == id) {
           return { ...el, qty: qty };
         }
-        return el
-      })
+        return el;
+      });
     },
     deleteCartItem(id) {
-      console.log('id: ', id);
-      this.cartItems = this.cartItems.filter(el => {
-        return el.id != id
-      })
+      console.log("id: ", id);
+      this.cartItems = this.cartItems.filter((el) => {
+        return el.id != id;
+      });
     },
-  }
-})
+  },
+});
